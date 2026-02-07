b1fd9a9beab1d9326c80a9c97e853a4fa5018481
Oussama Berrazaga <oussema.berrazaga@gmail.com>
2026-02-07 20:38:56 +0100

    Change package names and add discovery service configuration

--- services/config-server/src/main/java/com/skillbridge/configserver/ConfigServerApplication.java
+++ services/config-server/src/main/java/com/skillbridge/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.skillbridge.config_server;
+package com.skillbridge.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
