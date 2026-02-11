e1144b428ac1b4d86887acc33721492fb2f56f42
Oussama Berrazaga <oussema.berrazaga@gmail.com>
2026-02-11 09:38:13 +0100

    Add CRUD to category controller and add sample data for testing and add Jpa Auditing

--- services/listing-service/src/main/java/com/skillbridge/category/CategoryController.java
+++ services/listing-service/src/main/java/com/skillbridge/category/CategoryController.java
@@ -9,7 +9,10 @@ import lombok.RequiredArgsConstructor;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,10 +26,25 @@ public class CategoryController {
     return ResponseEntity.ok(service.getAllCategories());
   }
 
-  // @GetMapping("/{categoryId}")
-  // public ResponseEntity<CategoryResponse>
-  // getCategoryById(@PathParam("categoryId") Long categoryId) {
-  // return ResponseEntity.ok(service.getCategoryById(categoryId));
-  // }
+  @GetMapping("/{categoryId}")
+  public ResponseEntity<CategoryResponse> getCategoryById(@PathParam("categoryId") Long categoryId) {
+    return ResponseEntity.ok(service.getCategoryById(categoryId));
+  }
 
+  @PostMapping
+  public ResponseEntity<CategoryResponse> createCategory(CategoryRequest request) {
+    return ResponseEntity.ok(service.createCategory(request));
+  }
+
+  @PutMapping("/{categoryId}")
+  public ResponseEntity<CategoryResponse> updateCategory(@PathParam("categoryId") Long categoryId,
+      CategoryRequest request) {
+    return ResponseEntity.ok(service.updateCategory(categoryId, request));
+  }
+
+  @DeleteMapping("/{categoryId}")
+  public ResponseEntity<Void> deleteCategory(@PathParam("categoryId") Long categoryId) {
+    service.deleteCategory(categoryId);
+    return ResponseEntity.noContent().build();
+  }
 }
